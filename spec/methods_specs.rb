efc311909421e0ec6a3f36cfb3c204e6bb1560af
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-08-06 18:40:56 +0200

    add second test

--- spec/methods_specs.rb
+++ spec/methods_specs.rb
@@ -1,13 +1,19 @@
 require_relative '../lib/methods'
 
 describe Enumerable do
-  let(:array) {Array.new(5){|i| i}}
-  let(:range) {Range.new(1, 5)}
-  describe "#my_each" do
-    context "using it with array" do
-      context "without block" do
-        it "returns enumerator" do
+  let(:array) { Array.new(5) { |i| i } }
+  let(:range) { Range.new(1, 5) }
+  let(:hash) { { one: '1', two: '2' } }
+  describe '#my_each' do
+    context 'using it with array' do
+      context 'without block' do
+        it 'returns enumerator' do
           expect(array.my_each).to be_kind_of(Enumerator)
+        end
+      end
+      context 'with a block' do
+        it 'returns enumerator' do
+          expect(array.my_each { |i| puts i }).to eq(array.each { |i| puts i })
         end
       end
     end
