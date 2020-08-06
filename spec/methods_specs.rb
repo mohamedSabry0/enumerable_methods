baa7d7a35100b23ca161ba71bf7e7c57c4e7eea9
Mohamed Sabry <mohamedsabry3023@gmail.com>
2020-08-06 16:19:25 +0000

    fix require statement for tested file

--- spec/methods_specs.rb
+++ spec/methods_specs.rb
@@ -1,14 +1,13 @@
-require 'lib/methods'
+require_relative '../lib/methods'
 
 describe Enumerable do
   let(:array) {Array.new(5){|i| i}}
   let(:range) {Range.new(1, 5)}
-  discribe "#my_each" do
-
+  describe "#my_each" do
     context "using it with array" do
       context "without block" do
         it "returns enumerator" do
-          expect(array.my_each).to eq array.each
+          expect(array.my_each).to be_kind_of(Enumerator)
         end
       end
     end
