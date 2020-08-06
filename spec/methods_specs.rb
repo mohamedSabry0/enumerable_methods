136f7113be477c811e3a4361403285b3689326cf
mohamedSabry0 <mohamedsabry3023@gmail.com>
2020-08-06 17:40:00 +0200

    trying my first test

--- spec/methods_specs.rb
+++ spec/methods_specs.rb
@@ -1,5 +1,16 @@
 require 'lib/methods'
 
 describe Enumerable do
-  describe "#my_each" do
+  let(:array) {Array.new(5){|i| i}}
+  let(:range) {Range.new(1, 5)}
+  discribe "#my_each" do
+
+    context "using it with array" do
+      context "without block" do
+        it "returns enumerator" do
+          expect(array.my_each).to eq array.each
+        end
+      end
+    end
+  end
 end
